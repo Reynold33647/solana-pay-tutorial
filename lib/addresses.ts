3f5aa893cf7cc0613779031daac68b1d2c8419dd
Reynold33647 <trujrt65463486@protonmail.com>
2022-04-15 13:13:04 +0900

    04

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -4,3 +4,5 @@ import { PublicKey } from "@solana/web3.js"
 export const shopAddress = new PublicKey('G6g6K4fGTrUmvknZkKzJhAdcKT2nuYkbDXoja9UKyGKp') 
 // this is the same for everyone!
 export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+// This is your token/coupon address
+export const couponAddress = new PublicKey('AC7XjvEXf7nxQJzqaMBjqfD3hvjaMxyUk8KFHcu3opWd')
